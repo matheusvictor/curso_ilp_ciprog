133ac1cfe5bc5741a627e7745ad540da9f6fdaf7
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-15 21:32:47 -0300

    Adiciona coleção para simular banco de pessoas

---
+++ exercicios/colecoes/banco_de_pessoas.py
@@ -0,0 +1,179 @@
+pessoas = [
+    {
+        "nome": "João das Neves",
+        "idade": 33,
+        "sexo": "M",
+        "estado_civil": "Casado",
+        "área": "Saúde",
+        "profissão": "Médico",
+        "salário": 7000,
+        "dependentes": [
+            {
+                "nome": "Alice das Neves",
+                "sexo": "F",
+                "idade": 12,
+                "parentesco": "Filha"
+            }
+        ]
+    },
+    {
+        "nome": "Ana Müller",
+        "idade": 20,
+        "sexo": "F",
+        "estado_civil": "Solteira",
+        "área": "Tecnologia",
+        "profissão": "Estagiária",
+        "salário": 1500,
+        "dependentes": []
+    },
+    {
+        "nome": 'Joanna D\'Arc',
+        "idade": 30,
+        "sexo": "F",
+        "estado_civil": "Casada",
+        "área": "Direito",
+        "profissão": "Advogada",
+        "salário": 3200,
+        "dependentes": []
+    },
+    {
+        "nome": "Adelino da Silva",
+        "idade": 47,
+        "sexo": "M",
+        "estado_civil": "Divorciado",
+        "área": "Empreendimento",
+        "profissão": "Vendedor",
+        "salário": 800,
+        "dependentes": []
+    },
+    {
+        "nome": "Roberto Luiz",
+        "idade": 28,
+        "sexo": "M",
+        "estado_civil": "Solteiro",
+        "área": "Educação",
+        "profissão": "Professor",
+        "salário": 1600,
+        "dependentes": []
+    },
+    {
+        "nome": "Marcos Antonio",
+        "idade": 31,
+        "sexo": "M",
+        "estado_civil": "Casado",
+        "área": "Educação",
+        "profissão": "Professor",
+        "salário": 1600,
+        "dependentes": [
+            {
+                "nome": "Sergio",
+                "sexo": "M",
+                "idade": 5,
+                "parentesco": "Filho"
+            }
+        ]
+    },
+    {
+        "nome": "Luisa Vieira",
+        "idade": 41,
+        "sexo": "F",
+        "estado_civil": "Solteira",
+        "área": "Ciências",
+        "profissão": "Engenheira Química",
+        "salário": 3500,
+        "dependentes": [
+            {
+                "nome": "Beatriz Vieira",
+                "sexo": "F",
+                "idade": 20,
+                "parentesco": "Filha"
+            },
+            {
+                "nome": "Lucas",
+                "sexo": "M",
+                "idade": 17,
+                "parentesco": "Filho"
+            }
+        ]
+    },
+    {
+        "nome": "Amanda Cerqueira",
+        "idade": 35,
+        "sexo": "F",
+        "estado_civil": "Solteira",
+        "área": "Tecnologia",
+        "profissão": "Desenvolvedora",
+        "salário": 3500,
+        "dependentes": []
+    },
+    {
+        "nome": "Carlos Alberto",
+        "idade": 22,
+        "sexo": "M",
+        "estado_civil": "Solteiro",
+        "área": "Tecnologia",
+        "profissão": "Desenvolvedor",
+        "salário": 3500,
+        "dependentes": []
+    },
+    {
+        "nome": "Silvana Andrade",
+        "idade": 28,
+        "sexo": "F",
+        "estado_civil": "Casada",
+        "área": "Saúde",
+        "profissão": "Médica",
+        "salário": 5000,
+        "dependentes": []
+    },
+    {
+        "nome": "Pamela Amorim",
+        "idade": 25,
+        "sexo": "F",
+        "estado_civil": "Solteira",
+        "área": "Saúde",
+        "profissão": "Pediatra",
+        "salário": 1800,
+        "dependentes": []
+    },
+    {
+        "nome": "Pamela Amorim",
+        "idade": 25,
+        "sexo": "F",
+        "estado_civil": "Solteira",
+        "área": "Saúde",
+        "profissão": "Pediatra",
+        "salário": 1800,
+        "dependentes": []
+    },
+    {
+        "nome": "Paulo Roberto",
+        "idade": 32,
+        "sexo": "M",
+        "estado_civil": "Divorciado",
+        "área": "Direito",
+        "profissão": "Advogado",
+        "salário": 2800,
+        "dependentes": []
+    },
+    {
+        "nome": "Juliana Camarim",
+        "idade": 17,
+        "sexo": "F",
+        "estado_civil": "Solteira",
+        "área": "Saúde",
+        "profissão": "Estagiária",
+        "salário": 800,
+        "dependentes": []
+    },
+    {
+        "nome": "Carlos",
+        "idade": 25,
+        "sexo": "M",
+        "estado_civil": "Solteiro",
+        "área": None,
+        "profissão": None,
+        "salário": 0,
+        "dependentes": []
+    }
+]
